017c3ce7c6101f46a16cf4dc8eb02d50e4e78ea9
DenisLuba <denislubapershins@gmail.com>
2024-04-26 00:25:43 +0300

    Isograms

--- CodeWars.Tests/CorrectMistakesOfCharacterRecognitionTest.cs
+++ CodeWars.Tests/CorrectMistakesOfCharacterRecognitionTest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeWars.Tests
+namespace CodeWars.Test
 {
     internal class CorrectMistakesOfCharacterRecognitionTest
     {
