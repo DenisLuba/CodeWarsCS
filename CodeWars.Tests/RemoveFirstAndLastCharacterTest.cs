a10f45f8a9b7b0555b317d70c6597c9b48989cd8
DenisLuba <denislubapershins@gmail.com>
2024-04-26 23:35:55 +0300

    RemoveFirstAndLastCharacter

---
+++ CodeWars.Tests/RemoveFirstAndLastCharacterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Test
+{
+    [TestFixture]
+    internal class RemoveFirstAndLastCharacterTest
+    {
+        [TestCase("loquen", "eloquent")]
+        [TestCase("ountr", "country")]
+        [TestCase("erso", "person")]
+        [TestCase("lac", "place")]
+        [TestCase("", "ok")]
+        public void Test(string expected, string input)
+        {
+            Assert.That(RemoveFirstAndLastCharacter.Remove_char(input), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CodeWars/RemoveFirstAndLastCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class RemoveFirstAndLastCharacter
+    {
+        //public static string Remove_char(string s) => string.Concat(s.Skip(1).SkipLast(1));
+        public static string Remove_char(string s) => s[1..^1];
+    }
+}
