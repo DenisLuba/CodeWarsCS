8b9146af20674c2d23020e67070a1dfd5131c5dd
DenisLuba <denislubapershins@gmail.com>
2024-05-13 22:53:40 +0300

    etectPangram

---
+++ CodeWars.Tests/DetectPangramTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class DetectPangramTest
+    {
+        [TestCase("The quick brown fox jumps over the lazy dog.", true)]
+        public void Test(string text, bool expected)
+        {
+            Assert.That(DetectPangram.IsPangram(text), Is.EqualTo(expected));
+        }
+    }
+}
