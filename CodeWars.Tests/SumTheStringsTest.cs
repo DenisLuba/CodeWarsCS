00084da0e2b6c8c99732c29350aa3cdd6d8a3dd9
DenisLuba <denislubapershins@gmail.com>
2024-06-02 12:00:07 +0300

    SumTheStrings

---
+++ CodeWars.Tests/SumTheStringsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class SumTheStringsTest
+    {
+        [TestCase("1", "2", "3")]
+        [TestCase("", "" , "0")]
+        [TestCase("ABC", "2", "2")]
+        [TestCase("0", "b", "0")]
+        public static void Test(string a, string b, string expected)
+        {
+            Assert.That(SumTheStrings.StringsSum(a, b), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CodeWars/SumTheStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class SumTheStrings
+    {
+        public static string StringsSum(string a, string b)
+            => ((int.TryParse(a, out int numberA) ? numberA : 0) + (int.TryParse(b, out int numberB) ? numberB : 0)).ToString();
+    }
+}
