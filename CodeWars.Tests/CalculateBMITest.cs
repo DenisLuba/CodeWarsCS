f8b468859d80b0e2c7761a79de72b00d08daec31
DenisLuba <denislubapershins@gmail.com>
2024-06-02 11:34:42 +0300

    CalculateBMITest

---
+++ CodeWars.Tests/CalculateBMITest.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class CalculateBMITest
+    {
+        [TestCase(100, 1.5, "Obese")]
+        [TestCase(100, 1.9, "Overweight")]
+        [TestCase(100, 2, "Normal")]
+        [TestCase(100, 2.5, "Underweight")]
+        public static void Test(double weight, double height, string expected)
+            => Assert.That(CalculateBMI.Bmi(weight, height), Is.EqualTo(expected));    
+    }
+}
