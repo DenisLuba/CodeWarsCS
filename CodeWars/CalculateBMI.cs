678f0d8975457435dacb1951a226fbe1f5fbc5e5
DenisLuba <denislubapershins@gmail.com>
2024-06-02 11:32:12 +0300

    CalculateBMI

---
+++ CodeWars/CalculateBMI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class CalculateBMI
+    {
+        public static string Bmi(double weight, double height)
+        {
+            var bmi = weight / (height * height);
+            return bmi <= 18.5 ? "Underweight"
+                : bmi <= 25 ? "Normal"
+                : bmi <= 30 ? "Overweight"
+                : "Obese";
+        }
+    }
+}
