afbd58d4b8140242bfbd95cd9461f84c752a1508
DenisLuba <denislubapershins@gmail.com>
2024-06-06 22:48:43 +0300

    Bango

---
+++ CodeWars.Tests/BangoTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class BangoTest
+    {
+        [TestCase("Martin", "Martin does not play banjo")]
+        [TestCase("Rikke", "Rikke plays banjo")]
+        [TestCase("rolf", "rolf plays banjo")]
+        public void Test(string name, string expected)
+        {
+            Assert.That(Bango.AreYouPlayingBanjo(name), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CodeWars/Bango.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class Bango
+    {
+        public static string AreYouPlayingBanjo(string name)
+            => name.ToLower().StartsWith('r') ? $"{name} plays banjo" : $"{name} does not play banjo";
+    }
+}
