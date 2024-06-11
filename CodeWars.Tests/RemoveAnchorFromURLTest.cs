24f3508959bafc51b7df55669ad756965834fcfd
DenisLuba <denislubapershins@gmail.com>
2024-06-12 00:10:59 +0300

    Remove the anchor from URL

---
+++ CodeWars.Tests/RemoveAnchorFromURLTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class RemoveAnchorFromURLTest
+    {
+        [TestCase("", "")]
+        public void Test(string url, string expected)
+        {
+            Assert.That(RemoveAnchorFromURL.RemoveUrlAnchor("www.codewars.com#about"), Is.EqualTo("www.codewars.com"));
+            Assert.That(RemoveAnchorFromURL.RemoveUrlAnchor("www.codewars.com/katas/?page=1#about"), Is.EqualTo("www.codewars.com/katas/?page=1"));
+            Assert.That(RemoveAnchorFromURL.RemoveUrlAnchor("www.codewars.com/katas/"), Is.EqualTo("www.codewars.com/katas/"));
+        }
+    }
+}
---
+++ CodeWars/RemoveAnchorFromURL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class RemoveAnchorFromURL
+    {
+        //public static string RemoveUrlAnchor(string url) => new Regex(@"(?=.*)#.*").Replace(url, "");
+
+        //public static string RemoveUrlAnchor(string url) => Regex.Match(url, @"[^#]+").Value;
+
+        public static string RemoveUrlAnchor(string url) => url.Split("#")[0];
+    }
+}
