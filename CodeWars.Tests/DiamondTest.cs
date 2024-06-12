2ce7a2feea4703e15854decf5a447a955f638fc5
DenisLuba <denislubapershins@gmail.com>
2024-06-12 15:26:03 +0300

    Diamond

---
+++ CodeWars.Tests/DiamondTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class DiamondTest
+    {
+        [TestCase(5, "  *\n ***\n*****\n ***\n  *\n")]
+        [TestCase(3, " *\n***\n *\n")]
+        [TestCase(1, "*\n")]
+        // Tests null
+        [TestCase(-1, null)]
+        [TestCase(0, null)]
+        [TestCase(2, null)]
+
+        public void Test(int n, string? expected)
+            => Assert.That(Diamond.Print(n), Is.EqualTo(expected));
+        
+    }
+}
---
+++ CodeWars/Diamond.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    public class Diamond
+    {
+        public static string? Print(int n) 
+            => n > 0 && n % 2 != 0
+            ? string
+                .Concat((Enumerable.Range(1, n).Where(i => i % 2 != 0)).Concat(Enumerable.Range(1, n - 1).Where(i => i % 2 != 0).Reverse())
+                    .Select(i => $"{new string(' ', (n - i) / 2)}{new string('*', i)}\n"))
+            : null;
+    }
+}
