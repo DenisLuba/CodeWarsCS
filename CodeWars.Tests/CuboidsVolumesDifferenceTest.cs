a534f1b24c289ab5a5d88164c557a7b979c6727f
DenisLuba <denislubapershins@gmail.com>
2024-06-13 23:07:17 +0300

    CuboidsVoumesDifference

---
+++ CodeWars.Tests/CuboidsVolumesDifferenceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class CuboidsVolumesDifferenceTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { 3, 2, 5 }, new[] { 1, 4, 4 }).Returns(14);
+                yield return new TestCaseData(new[] { 9, 7, 2 }, new[] { 5, 2, 2 }).Returns(106);
+                yield return new TestCaseData(new[] { 11, 2, 5 }, new[] { 1, 10, 8 }).Returns(30);
+                yield return new TestCaseData(new[] { 4, 4, 7 }, new[] { 3, 9, 3 }).Returns(31);
+                yield return new TestCaseData(new[] { 15, 20, 25 }, new[] { 10, 30, 25 }).Returns(0);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(int[] a, int[] b) => CuboidsVolumesDifference.FindDifference(a, b);
+    }
+}
---
+++ CodeWars/CuboidsVolumesDifference.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class CuboidsVolumesDifference
+    {
+        public static int FindDifference(int[] a, int[] b) => Math.Abs(a.Product() - b.Product());
+    }
+
+    static class ArrayExtension
+    {
+        public static int Product(this int[] cuboid) => cuboid.Aggregate((x, y) => x * y);
+    }
+}
