28217e5e86ea48ee6acd22c0fb6ca806dc2e0cad
DenisLuba <denislubapershins@gmail.com>
2024-06-15 17:26:05 +0300

    Welcome method

---
+++ CodeWars.Tests/WelcomeTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class WelcomeTest
+    {
+        [TestCase("englich", "Welcome")]
+        [TestCase("dutch", "Welkom")]
+        [TestCase("IP_ADDRESS_INVALID", "Welcome")]
+        [TestCase("", "Welcome")]
+        [TestCase("2", "Welcome")]
+        public void Test(string language, string expected)
+            => Assert.That(Welcome.Greet(language), Is.EqualTo(expected));
+    }
+}
---
+++ CodeWars/Welcome.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class Welcome
+    {
+        private static Dictionary<string, string> WelcomeDB = new Dictionary<string, string>
+        { 
+            { "english", "Welcome" },
+            { "czech", "Vitejte" },
+            { "danish", "Velkomst" },
+            { "dutch", "Welkom" },
+            { "estonian", "Tere tulemast" },
+            { "finnish", "Tervetuloa" },
+            { "flemish", "Welgekomen" },
+            { "french", "Bienvenue" },
+            { "german", "Willkommen" },
+            { "irish", "Failte" },
+            { "italian", "Benvenuto" },
+            { "latvian", "Gaidits" },
+            { "lithuanian", "Laukiamas" },
+            { "polish", "Witamy" },
+            { "spanish", "Bienvenido" },
+            { "swedish", "Valkommen" },
+            { "welsh", "Croeso" },
+        };
+
+        public static string Greet(string language)
+            => WelcomeDB.TryGetValue(language, out var welcome) ? welcome : "Welcome";
+    }
+}
