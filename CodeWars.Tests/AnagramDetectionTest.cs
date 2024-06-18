27d01e471e7e37a50f0640e00a34fd64bfabe5d1
DenisLuba <denislubapershins@gmail.com>
2024-06-19 01:07:37 +0300

    AnagramDetection

---
+++ CodeWars.Tests/AnagramDetectionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class AnagramDetectionTest
+    {
+        [Test]
+        [TestCase("foefet", "toffee", ExpectedResult = true)]
+        [TestCase("Buckethead", "DeathCubeK", ExpectedResult = true)]
+        [TestCase("Twoo", "Woot", ExpectedResult = true)]
+        [TestCase("apple", "pale", ExpectedResult = false)]
+        public static bool Test(string test, string original)
+        {
+            return AnagramDetection.IsAnagram(test, original);
+        }
+    }
+}
---
+++ CodeWars/AnagramDetection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class AnagramDetection
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            return test.ToLower().OrderBy(x => x).SequenceEqual(original.ToLower().OrderBy(x => x));
+
+            //original = original.ToLower();
+
+            //bool result = test.ToLower().All(ch =>
+            //{
+            //    if (original.Contains(ch))
+            //    {
+            //        original = original.Remove(original.IndexOf(ch), 1);
+            //        return true;
+            //    }
+            //    return false;
+            //});
+
+            //return result && original.Length == 0;
+        }
+    }
+}
