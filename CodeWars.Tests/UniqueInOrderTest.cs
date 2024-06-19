89157aa656cb8aed96cc0397f920d706b75cf738
DenisLuba <denislubapershins@gmail.com>
2024-06-19 13:27:31 +0300

    UniqueInOrder

---
+++ CodeWars.Tests/UniqueInOrderTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    internal class UniqueInOrderTest
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.That(UniqueInOrder.UniqueInOrderMethod(""), Is.EqualTo(""));
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.That(UniqueInOrder.UniqueInOrderMethod("AAAABBBCCDAABBB"), Is.EqualTo("ABCDAB"));
+        }
+    }
+}
---
+++ CodeWars/UniqueInOrder.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+namespace CodeWars
+{
+    public static class UniqueInOrder
+    {
+        public static IEnumerable<T> UniqueInOrderMethod<T>(this IEnumerable<T> iterable)
+            => iterable.Where((t, i) => i == 0 || !Equals(t, iterable.ElementAt(i - 1)));
+
+        //public static IEnumerable<T> UniqueInOrderMethod<T>(IEnumerable<T> iterable)
+        //    => iterable
+        //    .Aggregate(new List<T>(),
+        //        (list, b) => list.GetList(b));
+
+        //private static List<T> GetList<T>(this List<T> list, T item)
+        //{
+        //    if (list.Count() != 0 && list.Last()!.Equals(item))
+        //        return list;
+        //    else
+        //    {
+        //        list.Add(item);
+        //        return list;
+        //    }
+        //}
+    }
+}
