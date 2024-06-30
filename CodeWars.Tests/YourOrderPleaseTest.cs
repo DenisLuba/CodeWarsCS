5bd50abc55ef5dab3c0bd04842a81c4fe61abe69
DenisLuba <denislubapershins@gmail.com>
2024-06-30 20:48:22 +0300

    YourOrderPlease

---
+++ CodeWars.Tests/YourOrderPleaseTest.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class YourOrderPleaseTest
+    {
+        [TestCase("is2 Thi1s T4est 3a", ExpectedResult = "Thi1s is2 3a T4est")]
+        [TestCase("4of Fo1r pe6ople g3ood th5e the2", ExpectedResult = "Fo1r the2 g3ood 4of th5e pe6ople")]
+        [TestCase("", ExpectedResult = "")]
+        public string Test(string words) => YourOrderPlease.Order(words);
+    }
+}
---
+++ CodeWars/YourOrderPlease.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWars
+{
+    public class YourOrderPlease
+    {
+        public static string Order(string words)
+            => string.Join(" ", words.Split(" ").OrderBy(w => Regex.Replace(w, @"\D", "")));
+    }
+}
