187fccaeccc291bae063e5b5db2ca76d80fc7dca
DenisLuba <denislubapershins@gmail.com>
2024-07-12 01:20:00 +0300

    VowelRemover

---
+++ CodeWars.Tests/VowelRemoverTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class VowelRemoverTest
+    {
+        [TestCase("hello", "hll")]
+        [TestCase("codewars", "cdwrs")]
+        [TestCase("goodbye", "gdby")]
+        [TestCase("HELLO", "HELLO")]
+        public void Test(string input, string expected)
+            => Assert.That(VowelRemover.Shortcut(input), Is.EqualTo(expected));
+    }
+}
---
+++ CodeWars/VowelRemover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars;
+
+public class VowelRemover
+{
+    private static char[] chars = ['a', 'e', 'i', 'o', 'u'];
+
+    public static string Shortcut(string input)
+        => string.Concat(
+            from ch in input
+            where !chars.Contains(ch)
+            select ch
+         );
+}
