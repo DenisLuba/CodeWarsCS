ca53cb442b2245e88d439642d2cd5ccca43c1cd3
DenisLuba <denislubapershins@gmail.com>
2024-07-12 01:10:05 +0300

    FindOddInt

---
+++ CodeWars.Tests/FindOddIntTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class FindOddIntTest
+    {
+        static IEnumerable<TestCaseData> caseDatas
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }).Returns(5);
+                yield return new TestCaseData(new[] { 0 }).Returns(0);
+                yield return new TestCaseData(new[] { 1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1 }).Returns(4);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(caseDatas))]
+        public int Test(int[] sequence) => FindOddInt.FindIt(sequence);
+
+        [Test, TestCaseSource(nameof(caseDatas))]
+        public int Test2(int[] sequence) => FindOddInt.FindIt2(sequence);
+    }
+}
---
+++ CodeWars/FindOddInt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars;
+
+public class FindOddInt
+{
+    public static int FindIt(int[] sequence)
+        => (from i in sequence
+            group i by i into g
+            where g.Count() % 2 != 0
+            select g.Key).Single();
+
+    public static int FindIt2(int[] sequence)
+        => sequence
+        .GroupBy(i => i)
+        .Single(g => g.Count() % 2 != 0)
+        .Key;
+
+}
+           
