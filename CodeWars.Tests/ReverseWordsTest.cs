bc82621742a6907e19831d87f18032f76ead20d2
DenisLuba <denislubapershins@gmail.com>
2024-07-14 00:27:27 +0300

    ReverseWords

---
+++ CodeWars.Tests/ReverseWordsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class ReverseWordsTest
+    {
+        [TestCase("", "")]
+        [TestCase("hello", "olleh")]
+        [TestCase("This is an example!", "sihT si na !elpmaxe")]
+        public void Test(string input, string expected)
+            => Assert.That(ReverseWords.GetReverseWords(input), Is.EqualTo(expected));
+    }
+}
---
+++ CodeWars/ReverseWords.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System;
+
+namespace CodeWars;
+
+public static class ReverseWords
+{
+    public static string GetReverseWords(string str)
+        => string.Join(
+            " ",
+            str.Split(' ').Select(word => word.ReverseArrayFramework()));
+
+    private static string ReverseArrayFramework(this string str)
+    {
+        char[] arr = str.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+}
