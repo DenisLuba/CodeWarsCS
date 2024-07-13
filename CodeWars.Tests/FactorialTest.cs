a40b660f21af00b7150219e33a92716d9a11ac07
DenisLuba <denislubapershins@gmail.com>
2024-07-14 00:50:37 +0300

    Factorial

---
+++ CodeWars.Tests/FactorialTest.cs
@@ -0,0 +1,20 @@
+﻿using NuGet.Frameworks;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    internal class FactorialTest
+    {
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 6)]
+        public void Test(int input, int expected) 
+            => Assert.That(Factorial.GetFactorial(input), Is.EqualTo(expected));
+
+        [TestCase(-5)]
+        [TestCase(13)]
+        public void Test(int input)
+            => Assert.That(() => Factorial.GetFactorial(input), Throws.ArgumentException);
+    }
+}
---
+++ CodeWars/Factorial.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    public class Factorial
+    {
+        public static int GetFactorial(int n)
+            => n >= 0 && n <= 12
+            ? Enumerable
+            .Range(1, n > 0 ? n : 1)
+            .Aggregate((n1, n2) => n1 * n2)
+            : throw new ArgumentException();
+    }
+}
