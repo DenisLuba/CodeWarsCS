d0b431c98b392170f9ecaeb09f742fe4a0b11766
DenisLuba <denislubapershins@gmail.com>
2024-07-16 03:48:16 +0300

    SupermarketQueue

---
+++ CodeWars.Tests/SupermarketQueueTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    internal class SupermarketQueueTest
+    {
+        [TestCase(new int[] { }, 1, 0)]
+        [TestCase(new[] { 1, 2, 3, 4 }, 1, 10)]
+        [TestCase(new[] { 2, 2, 3, 3, 4, 4 }, 2, 9)]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, 100, 5)]
+        public void Test(int[] customers, int n, long expected)
+            => Assert.That(SupermarketQueue.QueueTime(customers, n), Is.EqualTo(expected));
+    }
+}
---
+++ CodeWars/SupermarketQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class SupermarketQueue
+    {
+
+        public static long QueueTime(int[] customers, int n)
+        {
+            int[] tills = new int[n];
+            
+            foreach (int time in customers)
+                tills[Array.IndexOf(tills, tills.Min())] += time;
+            
+            return tills.Max();
+        }
+    }
+}
