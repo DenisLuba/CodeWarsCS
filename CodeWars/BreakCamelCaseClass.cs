816a5bc0a026843ec9c64f1211f415fb8b5c81e8
DenisLuba <denislubapershins@gmail.com>
2024-07-20 22:47:50 +0300

    BreakCamelCase

---
+++ CodeWars/BreakCamelCaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars;
+
+public class BreakCamelCaseClass
+{
+    public static string BreakCamelCase(string str) => str
+        .Aggregate("", (ch1, ch2) => char.IsUpper(ch2) ? $"{ch1} {ch2}" : $"{ch1}{ch2}");
+}
