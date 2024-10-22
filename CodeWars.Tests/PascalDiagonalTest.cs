6dade2765f3f4c8d8e2a47aa629d6731486bfcbe
DenisLuba <denislubapershins@gmail.com>
2024-10-22 17:06:50 +0300

    PascalDiagonal

---
+++ CodeWars.Tests/PascalDiagonalTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace CodeWars.Tests;
+
+[TestFixture]
+internal class PascalDiagonalTest
+{
+	static IEnumerable<TestCaseData> cases
+	{
+		get
+		{
+			yield return new TestCaseData(20, 3).Returns(new BigInteger(5985));
+			yield return new TestCaseData(20, 4).Returns(new BigInteger(20349));
+			yield return new TestCaseData(20, 5).Returns(new BigInteger(54264));
+		}
+	}
+
+	[Test, TestCaseSource(nameof(cases))]
+	public BigInteger Test(int row, int pos) => PascalDiagonal.Diagonal(row, pos);
+}
---
+++ CodeWars/PascalDiagonal.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace CodeWars;
+
+public class PascalDiagonal
+{
+	public static BigInteger Diagonal(int n, int k) => Binominal(n + 1, k + 1);
+	// public static BigInteger Diagonal(int n, int k) => Factorial(n + 1) / (Factorial(k + 1) * Factorial(n - k));
+
+	static BigInteger Factorial(int n) 
+		=> Enumerable
+		.Range(1, n)
+		.Aggregate(new BigInteger(1), (f, i) => f * i);
+
+	static BigInteger Binominal (int row, int pos)
+	{
+		BigInteger koef = 1;
+		int i;
+		if (row - pos > pos)
+			pos = row - pos;
+		for (i = pos + 1; i <= row; i++)
+			koef = koef * i;
+		for (i = 1; i < (row - pos + 1); i++)
+			koef = koef / i;
+		return koef;
+	}
+}
