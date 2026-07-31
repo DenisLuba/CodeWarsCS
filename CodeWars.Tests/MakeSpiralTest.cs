b4ba54135d5737bb87b208809fb6137d825ae597
Denis <denislubapershins@gmail.com>
2026-08-01 02:35:15 +0300

    MakeSpiral

---
+++ CodeWars.Tests/MakeSpiralTest.cs
@@ -0,0 +1,46 @@
+﻿namespace CodeWars.Tests;
+
+public class MakeSpiralTest
+{
+    static IEnumerable<TestCaseData> TestCases
+    {
+        get
+        {
+            yield return new TestCaseData(5).Returns(new int[,] {
+                {1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 1},
+                {1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1}
+            });
+            yield return new TestCaseData(6).Returns(new int[,]
+            {
+                {1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 0, 1},
+                {1, 0, 0, 1, 0, 1},
+                {1, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1}
+            });
+            yield return new TestCaseData(8).Returns(new int[,] {
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 1, 0, 1},
+                {1, 0, 1, 0, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1},
+            });
+            yield return new TestCaseData(1).Returns(new int[,] {
+                { 1 }
+            });
+        }
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public int[,] SpiralizeTest(int size)
+    {
+        return MakeSpiral.Spiralize(size);
+    }
+}
---
+++ CodeWars/MakeSpiral.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.AccessControl;
+
+namespace CodeWars;
+
+public class MakeSpiral
+{
+    static readonly int Gap = 2;
+
+    public static int[,] Spiralize(int size)
+    {
+        var spiral = new int[size,size];
+        int layer = 0, top = 0, left = 0, bottomExclusive = size, rightExclusive = size;
+
+        while (bottomExclusive > top)
+        {
+
+            FillRow(spiral, top, Math.Max(0, left - Gap), rightExclusive); // top side
+            FillColumn(spiral, left, top + Gap, bottomExclusive); // left side
+            FillColumn(spiral, rightExclusive - 1, top, bottomExclusive); // right side
+
+            if (bottomExclusive - top == Gap) break;
+
+            FillRow(spiral, bottomExclusive - 1, left, rightExclusive); // bottom side
+
+            layer += Gap;
+
+            top = left = layer;
+            bottomExclusive = rightExclusive = size - layer;
+        }
+
+        return spiral;
+    }
+
+    private static void FillRow(int[,] spiral, int row, int start, int end)
+    {
+        for (int i = start; i < end; i++)
+        {
+            spiral[row, i] = 1;
+        }
+    }
+
+    private static void FillColumn(int[,] spiral, int column, int start, int end)
+    {
+        for (int i = start; i < end; i++)
+        {
+            spiral[i, column] = 1;
+        }
+    }
+}
