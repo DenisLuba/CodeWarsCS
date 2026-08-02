986122ebebee321e3303531daf2cad2ac092dece
Denis <denislubapershins@gmail.com>
2026-08-03 00:50:09 +0300

    Started the LinearEquations

---
+++ CodeWars.Tests/LinearEquationsTest.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeWars.Tests;
+
+[TestFixture]
+public class LinearEquationsTest
+{
+    [TestCase("1 2 3 4\n6 6 7 8\n9 10 11 12", "SOL=(0; -1; 2)")]
+    [TestCase("1 2 3 4", "SOL=(4; 0; 0) + q1 * (-2; 1; 0) + q2 * (-3; 0; 1)")]
+    [TestCase("3/2 1/2 3", "SOL=(2; 0) + q1 * (-1/3; 1)")]
+    [TestCase("1 2 2\n1 2 2\n2 4 4", "SOL = (2; 0) + q1 * (-2; 1)")]
+    [TestCase("0 0 0\n0 0 0", "SOL = (0; 0) + q1 * (1; 0) + q2 * (0; 1)")]
+    [TestCase("0 0 0 0\n0 0 0 0", "SOL = (0; 0; 0) + q1 * (1; 0; 0) + q2 * (0; 1; 0) + q3 * (0; 0; 1)")]
+    [TestCase("1/20 -10/3 -10/9 -13\n-29 8 -27/4 0\n-26 -14 25 10/7", "SOL=(3343180/9270107; 4197595/1324301; 20461200/9270107)")]
+    [TestCase("0 0 1 2 1\n1 2 1 3 1\n1 2 2 5 3", "SOL=NONE")]
+    public void SimpleTest(string input, string output)
+        => Assert.That(LinearEquations.Solve(input), Is.EqualTo(output));
+
+}
---
+++ CodeWars/LinearEquations.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeWars;
+
+public class LinearEquations
+{
+    public static string Solve(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
