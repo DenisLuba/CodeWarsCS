5df7e7e8686621ea7c61b48829398da4f3ba78b3
Denis <denislubapershins@gmail.com>
2026-08-06 23:02:08 +0300

    SetEchelonFormMatrix in the LinearEquatiions

--- CodeWars/LinearEquations.cs
+++ CodeWars/LinearEquations.cs
@@ -4,32 +4,65 @@ public class LinearEquations
 {
     public static string Solve(string input)
     {
-        var matrix = GetMatrix(input);
+        var matrix = GetMatrixFromString(input);
     }
 
     static Fraction[][] SolveByGauss(Fraction[][] matrix)
     {
-        int rows = matrix[0].Length - 1;
-
-        // неправильно. Надо поправить, добавить еще проходку внутри
-        //foreach (var row in matrix)
-        //{
-        //    var x = row[0];
-
-        //    if (x.Numerator != x.Denominator || x.Numerator != 0)
-        //    {
-        //        var factor = new Fraction(x.Denominator, x.Numerator);
-
-        //        foreach (var i in row)
-        //        {
-        //            i.Numerator *= factor.Numerator;
-        //            i.Denominator *= factor.Denominator;
-        //        }
-        //    }
-        //}
+        SetEchelonFormMatrix(matrix);
     }
 
-    static Fraction[][] GetMatrix(string input)
+    static void SetEchelonFormMatrix(Fraction[][] matrix)
+    {
+        var factorFraction = new Fraction();
+
+        for (int k = 0; k < matrix.Length && k < matrix[0].Length - 1; k++)
+        {
+            for (int i = k; i < matrix.Length; i++)
+            {
+                factorFraction.Numerator = matrix[i][k] > 0 
+                    ? matrix[i][k].Denominator 
+                    : -matrix[i][k].Denominator;
+                factorFraction.Denominator = Math.Abs(matrix[i][k].Numerator);
+
+                for (int j = 0; j < matrix[i].Length - 1; j++)
+                {
+                    matrix[i][j] *= factorFraction;
+                }
+            }
+
+            if (k == 0) continue;
+
+            for (int i = k; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length - 1; j++)
+                {
+                    matrix[i][j] = matrix[i][j] - matrix[k - 1][j];
+                }
+            }
+        }
+    }
+
+    static Fraction[][] GetResult(Fraction[][] matrix)
+    {
+        var valuesNumber = matrix[0].Length - 1;
+
+        var result = new Fraction[valuesNumber][];
+
+        for (int i = matrix.Length - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < valuesNumber; j++)
+            {
+                if (matrix[i][j] == 0) continue;
+
+                for (int k = j + 1; k < valuesNumber; k++)
+                {
+
+                }
+            }
+    }
+
+    static Fraction[][] GetMatrixFromString(string input)
     {
         var lines = input.Split('\n', 'r', StringSplitOptions.RemoveEmptyEntries);
         Fraction[][] matrix = new Fraction[lines.Length][];
@@ -59,10 +92,87 @@ public class LinearEquations
         public int Numerator { get; set; } = numerator;
         public int Denominator { get; set; } = denominator == 0 ? throw new ArgumentException("Denominator cannot be zero") : denominator;
 
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            int numerator = a.Numerator * b.Denominator + b.Numerator * a.Denominator;
+            int denominator = a.Denominator * b.Denominator;
+            return new Fraction(numerator, denominator);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            int numerator = a.Numerator * b.Denominator - b.Numerator * a.Denominator;
+            int denominator = a.Denominator * b.Denominator;
+            return new Fraction(numerator, denominator);
+        }
+
+        public static Fraction operator -(Fraction f)
+        {
+            return new Fraction(-f.Numerator, f.Denominator);
+        }
+
+        public static bool operator ==(Fraction a, double b)
+        {
+            return a.Numerator / a.Denominator == b;
+        }
+
+        public static bool operator !=(Fraction a, double b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator <(Fraction a, double b)
+        {
+            return a.Numerator / a.Denominator < b;
+        }
+
+        public static bool operator >(Fraction a, double b)
+        {
+            return a.Numerator / a.Denominator > b;
+        }
+
+        public static Fraction Reduce(Fraction f)
+        {
+            int gcd = GetGCD(f.Numerator, f.Denominator);
+            return new Fraction(f.Numerator / gcd, f.Denominator / gcd);
+        }
+
+        static int GetGCD(int a, int b)
+        { 
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
         public override string ToString()
         {
             if (Denominator == 1) return Numerator.ToString();
             return $"{Numerator}/{Denominator}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Fraction other)
+            {
+                var reducedThis = Reduce(this);
+                var reducedOther = Reduce(other);
+                return reducedThis.Numerator == reducedOther.Numerator && reducedThis.Denominator == reducedOther.Denominator;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Numerator, Denominator);
         }
     }
 }
